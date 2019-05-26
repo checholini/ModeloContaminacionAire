75877f5e1a8dac1717620f89e836d8469cccd55b
checholini <sejrl3@gmail.com>
2019-05-26 18:41:07 -0500

    Modificacion del programa base

--- ModeloContaminacion/src/chart/XYSeriesDemo.java
+++ ModeloContaminacion/src/chart/XYSeriesDemo.java
@@ -22,31 +22,28 @@ public class XYSeriesDemo extends ApplicationFrame {
  *
  * @param title  the frame title.
  */
-    public XYSeriesDemo(final String title) {
+    public XYSeriesDemo(final String title, int caso) {
+        //Asigna un titulo a la ventana
         super(title);
-        double contaminacion = 0; 
-        final XYSeries series = new XYSeries("Contaminacion");
-        for (double i = 0; i < 10; i+=0.3) {
-            contaminacion = calcularValores(i);
-            System.out.println(contaminacion);
-            series.add(i, contaminacion);
+        //CRea la serie que va a graficar
+        final XYSeries serieTiempo = createSeries("Contaminacion");
+        // Hace un wrap de la serie
+        final XYSeriesCollection data = new XYSeriesCollection(serieTiempo);
+        // crea la grafica basado en los datos
+        if(caso == 1){
+                // Anade la grafica a una ventana
+                final JFreeChart ventanaTiempo = createChart(data,1 );
+                final ChartPanel chartPanelTiempo = new ChartPanel(ventanaTiempo); 
+                // Asigna un tamano a la ventana
+                chartPanelTiempo.setPreferredSize(new java.awt.Dimension(800, 450));
+                setContentPane(chartPanelTiempo);
+        } else {
+                 final JFreeChart ventanaXY = createChart(data,2 );
+                 final ChartPanel chartPanelXY = new ChartPanel(ventanaXY);
+                // Asigna un tamano a la ventana
+                chartPanelXY.setPreferredSize(new java.awt.Dimension(800, 450));
+                setContentPane(chartPanelXY);
         }
-        final XYSeriesCollection data = new XYSeriesCollection(series);
-        final JFreeChart chart = ChartFactory.createXYLineChart(
-            "Contaminacion del aire", 
-            "Tiempo", 
-            "Valor Contaminacion",
-            data,
-            PlotOrientation.VERTICAL,
-            true,
-            true,
-            false
-        );
-
-        final ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(800, 450));
-        setContentPane(chartPanel);
-
     }
 
 
@@ -57,16 +54,17 @@ public class XYSeriesDemo extends ApplicationFrame {
  */
     public static void main(final String[] args) {
 
-        final XYSeriesDemo demo = new XYSeriesDemo("Modelo Gauseano");
-        final XYSeriesDemo demo1 = new XYSeriesDemo("Modelo Gauseano");
-        demo.pack();
-        demo1.pack();
-        RefineryUtilities.centerFrameOnScreen(demo);
-        demo.setVisible(true);
-        demo1.setVisible(true);
+        final XYSeriesDemo tiempo = new XYSeriesDemo("Modelo Gauseano vs tiempo",1);
+        final XYSeriesDemo yvsz = new XYSeriesDemo("Modelo Gauseano y vs z",2);
+        tiempo.pack();
+        yvsz.pack();
+        RefineryUtilities.centerFrameOnScreen(tiempo);
+        tiempo.setVisible(true);
+        yvsz.setVisible(true);
 
     }
 
+    //Calcula los valores para la grafica 
     private double calcularValores(double i) {
         int k = 2000;//Constante de dispersion turbulenta
         int x = 1000;//Distancia Viento x
@@ -86,6 +84,57 @@ public class XYSeriesDemo extends ApplicationFrame {
         double X = 300;
         double contaminacion = (q/(2*Math.PI*u*v*dispercionX*dispercionY))*(Math.pow(Math.E, -(Y*Y)/(2*dispercionY*dispercionY))*(Math.pow(Math.E, -((X-h)*(X-h))/(2*dispercionX*dispercionX))));
         return contaminacion;
+    }
+
+    /**
+     * 
+     * Metodo para crear una serie
+     * @param String Identificador 
+     * @return Serie de valores para usar en el grafico
+     */
+    private XYSeries createSeries(String contaminacion) {
+        XYSeries series=  new XYSeries(contaminacion);
+        double iterador = 0;
+        for (double i = 0; i < 10; i+=0.3) {
+            iterador = calcularValores(i);
+            System.out.println(iterador);
+            series.add(i, iterador);
+        }
+        return series;
+    }
+
+    /**
+     * 
+     *  Metodo que permite crear el contenidio de la ventana
+     * @param data wraper de la serie
+     * @return Informacion para anadir a la ventana
+     */
+    private JFreeChart createChart(XYSeriesCollection data, int caso) {
+        JFreeChart chart; 
+        if(caso == 1){
+                chart = ChartFactory.createXYLineChart(
+                        "Contaminacion del aire", 
+                        "Tiempo", 
+                        "Valor Contaminacion",
+                        data,
+                        PlotOrientation.VERTICAL,
+                        true,
+                        true,
+                        false
+                );
+        } else {
+                chart = ChartFactory.createXYLineChart(
+                        "Contaminacion del aire", 
+                        "Y", 
+                        "Z",
+                        data,
+                        PlotOrientation.VERTICAL,
+                        true,
+                        true,
+                        false
+                );
+        }        
+        return chart;
     }
 
 }
